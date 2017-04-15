beafd6b34a6ba05e1b39825a252ca449a6d0c538
LittleCabbage <1262703491@qq.com>
2017-04-15 09:06:04 +0800

    用索引的方法来统计字符串中每个字符出现的次数

---
+++ fiftyprograms/src/com/seven/second/StringFilter3.java
@@ -0,0 +1,36 @@
+package com.seven.second;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class StringFilter3 {
+
+	public static void main(String[] args) {
+      System.out.println("请输入一行字符：");
+      Scanner sc = new Scanner(System.in);
+      String str = sc.nextLine();
+      sc.close();
+      count(str);
+	}
+
+	private static void count(String str) {
+
+		List<String> list = new ArrayList<>();
+		char[] ch = str.toCharArray();
+		for(char c : ch)
+			//将字符作为字符串添加到list表中
+			list.add(String.valueOf(c));
+		    //排序
+			Collections.sort(list);
+			for(String s : list){
+				int begin = list.indexOf(s);
+				int end = list.lastIndexOf(s);
+				//索引结束统计字符
+				if(list.get(end) == s){
+					System.out.print("字符‘"+s+"’有"+(end+1-begin)+"个"+"   ");
+				}
+			}
+		}
+}
