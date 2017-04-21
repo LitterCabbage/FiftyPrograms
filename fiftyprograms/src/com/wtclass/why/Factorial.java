5b1bb0005bbd8388b5c9db89062ed55ad3195392
LittleCabbage <1262703491@qq.com>
2017-04-21 17:10:50 +0800

    有关于阶乘的实现

---
+++ fiftyprograms/src/com/wtclass/why/Factorial.java
@@ -0,0 +1,63 @@
+package com.wtclass.why;
+
+import java.math.BigInteger;
+
+/**
+ * 阶乘的一些方法
+ *
+ * @author anxingxing
+ *
+ *         2017年4月21日  下午5:00:02
+ */
+public class Factorial {
+
+	public static void main(String[] args) {
+		mutiply_4(9);
+	}
+
+	/**
+	 * 阶乘     递归实现
+	 * @param n
+	 * @return
+	 */
+	public static int mutiply_1(int n){
+		if(n<=1){
+			return n;
+		}else{
+			return n*mutiply_1(n-1);
+		}
+	}
+	
+	/**
+	 * 阶乘     递归实现   增大数字范围
+	 * @param n
+	 * @return
+	 */
+	public static BigInteger mutiply_2(int n){
+		if(n<=1){
+			return BigInteger.valueOf(n);
+		}else{
+			return BigInteger.valueOf(n).multiply(mutiply_2(n-1));
+		}
+	}
+	
+	/**
+	 * 阶乘     非递归实现
+	 * @param n
+	 */
+	public static void mutiply_3(int n){
+		int sum = 1;
+		for (int i = 1; i <= n; i++) {
+			sum *= i; 
+		}
+		System.out.println(sum);
+	}
+	
+	public static void mutiply_4(int n){
+		BigInteger sum = BigInteger.valueOf(1);
+		for (int i = 1; i <= n; i++){
+			sum = sum.multiply(BigInteger.valueOf(i));
+		}
+		System.out.println(sum.toString());
+	}
+}
