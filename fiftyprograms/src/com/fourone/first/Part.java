00d4bc7319652b0a298d06eb81f0289cc808e56e
LittleCabbage <1262703491@qq.com>
2017-04-24 23:50:43 +0800

    整数分解

---
+++ fiftyprograms/src/com/fourone/first/Part.java
@@ -0,0 +1,29 @@
+package com.fourone.first;
+
+/**
+ * 将一个正整数分解质因数。例如：输入90,打印出90=2*3*3*5。
+ *
+ * @author anxingxing
+ *
+ *         2017年4月24日  下午11:48:49
+ */
+public class Part {
+
+	public static void main(String[] args) {
+		int n = 24;
+		decompose(n);
+	}
+	private static void decompose(int n){
+		System.out.print(n+"=");
+		for(int i=2;i<n+1;i++){
+			while(n%i==0 && n!=i){
+				n/=i;
+				System.out.print(i+"*");
+			}
+			if(n==i){
+				System.out.println(i);
+				break;
+			}
+		}
+	}
+}
