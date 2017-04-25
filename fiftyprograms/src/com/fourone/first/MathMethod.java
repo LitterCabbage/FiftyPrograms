c4728a673e7d5d4ac006739ea6176a0eef2f7b93
LittleCabbage <1262703491@qq.com>
2017-04-25 23:41:32 +0800

    完数

---
+++ fiftyprograms/src/com/fourone/first/MathMethod.java
@@ -0,0 +1,37 @@
+package com.fourone.first;
+
+import java.util.Scanner;
+
+/**
+ * 一个数如果恰好等于它的因子之和，这个数就称为"完数"。
+ * 例如6=1＋2＋3.编程找出1000以内的所有完数。 
+ *
+ * @author anxingxing
+ *
+ *         2017年4月25日  下午11:31:37
+ */
+public class MathMethod {
+
+	public static void main(String[] args){
+		int n = 10000;
+		compNumber(n);
+	}
+	//求完数
+	private static void compNumber(int n){
+		int count = 0;
+		System.out.println(n+"以内的完数：");
+		for(int i=1;i<n+1;i++){
+			int sum = 0;
+			for(int j=1;j<i/2+1;j++){
+				if((i%j)==0){
+					sum += j;
+					if(sum==i){
+				  System.out.print(i+" ");
+				  if((count++)%5==0)
+				    System.out.println();
+			    }
+				}
+			}
+		}
+	}
+}
